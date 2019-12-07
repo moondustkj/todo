245af0d683dd8d359255a9963903065765c83c0b
komaljituri <komaljituri88@gmail.com>
2019-12-07 10:17:58 +0530

    Updates

---
+++ precache-manifest.d403d96068fcb434f020b5d1cbb623fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "971c15553b0c458ba915",
+    "url": "/todo/static/js/main.cf271f80.chunk.js"
+  },
+  {
+    "revision": "52f6bf5e0fdd03d5e34d",
+    "url": "/todo/static/js/2.b7841a69.chunk.js"
+  },
+  {
+    "revision": "971c15553b0c458ba915",
+    "url": "/todo/static/css/main.e1512fc1.chunk.css"
+  },
+  {
+    "revision": "d1a20b52ea1230bf8915fb411662dba3",
+    "url": "/todo/index.html"
+  }
+];
